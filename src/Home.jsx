f89d6d58472099067c4ffd10cdb5727cdd232363
Peter Kim <petertimmmybob113@gmail.com>
2024-08-14 15:19:29 -0500

    Basic home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import './Home.css'
+
 function Home() {
   return (
-    <>
-      <h1>Add more stuff here later, main focus is the funtionality first</h1>
-    </>
+    <div className="home">
+      <h1>Welcome to RealFakeStore, a e-commerce site that is totally real!</h1>
+      <p>Here we have a vast array of items to fulfull your shopping needs!</p>
+      <h1>What are you waiting for?!</h1>
+      <Link to="/shop"><button>Explore now</button></Link>
+    </div>
   )
 }
 
